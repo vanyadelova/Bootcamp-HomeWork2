8461abc74b45a0c91e772147ebb08129b1ac68cb
vanyadelova <happy.vanya.delova@gmail.com>
2018-10-27 18:52:02 +0200

    new methods

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,5 +1,44 @@
 module.exports = class ShoppingCart {
+    constructor(cart, items) {
+        this.cart = []
+        this.items = []
+    }
+} 
+
+const cart = class {
+    constructor(items) {
+      this.items = items;
+      
+    }
+  }
+
+  class ShoppingCart {
     constructor() {
       this.items = []
     }
+
+    addItem(itemName, quantity, price) {
+        this.items.push({
+          name: itemName,
+          quantity: quantity,
+          pricePerUnit: price
+        })
+      }
+  
+    getItems() {
+      return this.items
+    } 
 } 
+
+clear() {
+    this.items = []
+    return this.items
+}
+
+total() {
+    return this.items
+      .map(item => item.pricePerUnit * item.quantity)
+      .reduce((acc, item) => acc + item, 0)
+  }
+
+
