7bc381172e82d54ab5a8831372340c7be4deeaf5
vanyadelova <happy.vanya.delova@gmail.com>
2018-10-27 22:58:10 +0200

    cleaning and formatting code

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,45 +1,45 @@
 module.exports = class ShoppingCart {
-    constructor(cart, items) {
-        this.cart = []
-        this.items = []
-    }
-} 
+  constructor(cart, items) {
+    this.cart = []
+    this.items = []
+  }
+}
 
 const cart = class {
-    constructor(items) {
-      this.items = items;
-      
-    }
+  constructor(items) {
+    this.items = items;
+
+  }
+}
+
+class ShoppingCart {
+  constructor() {
+    this.items = []
   }
 
-  class ShoppingCart {
-    constructor() {
-      this.items = []
-    }
+  addItem(itemName, quantity, price) {
+    this.items.push({
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit: price
+    })
+  }
 
-    addItem(itemName, quantity, price) {
-        this.items.push({
-          name: itemName,
-          quantity: quantity,
-          pricePerUnit: price
-        })
-      }
-  
-    getItems() {
-      return this.items
-    } 
-} 
+  getItems() {
+    return this.items
+  }
+}
 
 clear(); {
-    this.items = []
-    return this.items
+  this.items = []
+  return this.items
 }
 
 total(); {
-    return this.items
-      .map(item => item.pricePerUnit * item.quantity)
-      .reduce((acc, item) => acc + item, 0)
-  }
+  return this.items
+    .map(item => item.pricePerUnit * item.quantity)
+    .reduce((acc, item) => acc + item, 0)
+}
 
 
 module.exports = ShoppingCart;
