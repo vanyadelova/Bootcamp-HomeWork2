087c1d3db503e46c117991c6913853743649a5ce
vanyadelova <happy.vanya.delova@gmail.com>
2018-10-27 22:29:13 +0200

    filtering

--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,39 @@
 // Creating an exprting module:
 
-module.exports.groupAdultsByAgeRange = function groupAdultsByAgeRange 
+module.exports.groupAdultsByAgeRange = function groupAdultsByAgeRange(people){
+
+    const result = { '20 and younger': [ { name: 'John', age: 20 } ] }
+    const result2 = { 
+        '31-40': [ { name: 'Anna', age: 31 }, { name: 'John', age: 32 } ]}
+        
+        const result3 = { 
+            '41-50': [ { name: 'Maria', age: 45 }, { name: 'Daniel', age: 44 } ]}
+
+            const result4 = { 
+                '51 and older': [ { name: 'Hank', age: 60 }, { name: 'Peter', age: 52 } ]}
+
+            }
+           
+            let filtered = groupAdultsByAgeRange.filter(val => {
+                return val.age <= 20;
+              });
+
+              let filtered2 = groupAdultsByAgeRange.filter(val => {
+                return val.age <= 40 && val.age > 30;
+              });
+        
+              let filtered3 = groupAdultsByAgeRange.filter(val => {
+                return val.age <= 50 && val.age > 40;
+              });
+    
+
+              let filtered4 = groupAdultsByAgeRange.filter(val => {
+                return val.age >= 51;
+              });
+
+  
+
+
+
+
+
